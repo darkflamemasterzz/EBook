61494685041993df8bd76af4ceaa8acf54c7a7a0
Bin <m13678954738@gmail.com>
2020-04-23 09:40:21 +0800

    实现翻页功能

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -2,6 +2,11 @@
 <div class="ebook">
     <div class="read-wrapper">
         <div id="read"></div>
+        <div class="mask">
+            <div class="left" @click="prevPage"></div>
+            <div class="center"></div>
+            <div class="right" @click="nextPage"></div>
+        </div>
     </div>
 </div>
 </template>
@@ -30,6 +35,18 @@ export default {
           })
           // 通过Rendition.display渲染电子书
           this.rendition.display()
+      },
+      // 向前翻页
+      prevPage() {
+          if (this.rendition) {
+              this.rendition.prev()
+          }
+      },
+      // 向后翻页
+      nextPage() {
+          if (this.rendition) {
+              this.rendition.next()
+          }
       }
   },
   mounted() {
@@ -39,5 +56,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import './assets/styles/global.scss'
+@import './assets/styles/global.scss';
+.ebook{
+    position: relative;
+    .mask{
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 100;
+        width: 100%;
+        height: 100%;
+        display: flex;
+
+        .left{
+            flex: 0 0 px2rem(100);
+        }
+        .center{
+            flex: 1;
+        }
+        .right{
+            flex: 0 0 px2rem(100);
+        }
+    }
+}
 </style>
