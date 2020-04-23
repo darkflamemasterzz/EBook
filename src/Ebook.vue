a2c489a472e6d7d03c56e845e3a849e450e598ec
Bin <m13678954738@gmail.com>
2020-04-23 10:34:17 +0800

    实现菜单栏和标题栏的布局

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -1,11 +1,41 @@
 <template>
 <div class="ebook">
+    <div class="title-wrapper">
+        <div class="left">
+            <span class="icon-left icon"></span>
+        </div>
+        <div class="right">
+            <div class="icon-wrapper">
+                <span class="icon-cart icon"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon-user icon"></span>
+            </div>
+            <div class="icon-wrapper">
+                <span class="icon-more icon"></span>
+            </div>
+        </div>
+    </div>
     <div class="read-wrapper">
         <div id="read"></div>
         <div class="mask">
             <div class="left" @click="prevPage"></div>
             <div class="center"></div>
             <div class="right" @click="nextPage"></div>
+        </div>
+    </div>
+    <div class="menu-wrapper">
+        <div class="icon-wrapper">
+            <span class="icon-manage icon"></span>
+        </div>
+        <div class="icon-wrapper">
+            <span class="icon-progress icon"></span>
+        </div>
+        <div class="icon-wrapper">
+            <span class="icon-bright icon"></span>
+        </div>
+        <div class="icon-wrapper">
+            <span class="icon-A icon"></span>
         </div>
     </div>
 </div>
@@ -67,7 +97,6 @@ export default {
         width: 100%;
         height: 100%;
         display: flex;
-
         .left{
             flex: 0 0 px2rem(100);
         }
@@ -76,6 +105,61 @@ export default {
         }
         .right{
             flex: 0 0 px2rem(100);
+        }
+    }
+    .title-wrapper {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 101;
+        display: flex;
+        width: 100%;
+        height: px2rem(60);
+        background: white;
+        box-shadow: 0 px2rem(8) px2rem(8) rgba(0,0,0,.15);
+        .left{
+            flex: 0 0 px2rem(60);
+            @include center;
+            .icon{
+                font-size: px2rem(30);
+            }
+        }
+        .right{
+            flex: 1;
+            @include center;
+            display: flex;
+            justify-content: flex-end;
+            .icon-wrapper{
+                flex: 0 0 px2rem(40);
+                @include center;
+                .icon{
+                    font-size: px2rem(25);
+                }
+            }
+        }
+    }
+    .menu-wrapper{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 101;
+        display: flex;
+        width: 100%;
+        height: px2rem(60);
+        background: white;
+        box-shadow: 0 px2rem(-8) px2rem(8) rgba(0,0,0,.15);
+        .icon-wrapper{
+            flex: 1;
+            @include center;
+            .icon-manage {
+                font-size: px2rem(24)
+            }
+            .icon-progress {
+                font-size: px2rem(24)
+            }
+            .icon-bright {
+                font-size: px2rem(24)
+            }
         }
     }
 }
